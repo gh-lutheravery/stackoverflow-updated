9b173243237c74af44b06d4622b3cdadc3e813b7
gh-charles <blakos244@gmail.com>
2023-11-07 20:19:55 -0600

    make profile model

---
+++ WebApplication5/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication5.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PicturePath { get; set; }
+
+        public string? Bio { get; set; }
+
+        public List<Question>? Questions { get; set; }
+
+        public List<Answer>? Answers { get; set; }
+    }
+}
