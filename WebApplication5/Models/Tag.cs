9d6b8dc39e1aba848c11a6806de7fdcb3dc5f1b3
gh-charles <blakos244@gmail.com>
2023-11-07 20:20:01 -0600

    make tag mod

---
+++ WebApplication5/Models/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication5.Models
+{
+    public class Tag
+    {
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public int QuestionCount { get; set; }
+    }
+}
