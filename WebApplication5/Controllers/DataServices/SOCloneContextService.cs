c0528cded5c5928e4ec30b240615b794c4f312fd
gh-charles <blakos244@gmail.com>
2023-11-13 15:36:26 -0600

    make context service

---
+++ WebApplication5/Controllers/DataServices/SOCloneContextService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication5.Data;
+using WebApplication5.Models;
+using WebApplication5.ViewModels.User;
+
+namespace WebApplication5.Controllers.DataServices
+{
+    public class SOCloneContextService
+    {
+        private readonly StackOverflowCloneContext _context;
+
+        public SOCloneContextService(StackOverflowCloneContext context)
+        {
+            _context = context;
+        }
+
+        public Profile GetById(int id)
+        {
+            var profile = _context.Profile
+                .Include(p => p.Questions)
+                .Include(p => p.Answers)
+                .AsNoTracking()
+                .SingleOrDefault(p => p.Id == id);
+
+            return profile;
+        }
+
+        public void Create(Profile newProfile) 
+        {
+            _context.Profile.Add(newProfile);
+            _context.SaveChanges();
+        }
+    }
+}
