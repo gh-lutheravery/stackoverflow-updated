88d6cba81a4d61fdd8a8bab1cd37481d9b067d9b
gh-charles <blakos244@gmail.com>
2023-11-13 15:38:47 -0600

    make new home ctr

---
+++ WebApplication5/Controllers/WebControllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.ViewModels.Home;
+
+namespace WebApplication5.Controllers.WebControllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HomeViewModel vm = new HomeViewModel();
+            return View(vm);
+        }
+
+        public ActionResult SearchResults(string searchQuery)
+        {
+            SearchViewModel vm = new SearchViewModel();
+            return View(vm);
+        }
+    }
+}
