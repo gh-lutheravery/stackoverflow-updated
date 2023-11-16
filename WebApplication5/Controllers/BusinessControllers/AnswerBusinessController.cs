73c275f3ad568edfc6a75568ca3015a9c4c0a2b7
gh-charles <blakos244@gmail.com>
2023-11-15 18:33:16 -0600

    finish answer ctr

--- WebApplication5/Controllers/BusinessControllers/AnswerBusinessController.cs
+++ WebApplication5/Controllers/BusinessControllers/AnswerBusinessController.cs
@@ -1,41 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using WebApplication5.Controllers.DataServices;
 using WebApplication5.Models;
-using WebApplication5.ViewModels;
+using WebApplication5.ViewModels.QuestionAndAnswer;
 
 namespace WebApplication5.Controllers.BusinessControllers
 {
     public class AnswerBusinessController : Controller
     {
-        private SOCloneContextService _context;
+        private SOCloneContextService _contextService;
 
         public AnswerBusinessController(SOCloneContextService context)
         {
-            _context = context;
+            _contextService = context;
         }
 
-        public void SubmitAnswerForm()
+        public void SubmitAnswerForm(AnswerCreateViewModel answerForm, ClaimsPrincipal userCookie)
         {
-            return View();
+            Answer newAnswer = new Answer();
+
+            int userId = Int32.Parse(userCookie.FindFirstValue("ID"));
+            newAnswer.Author = _contextService.GetProfileById(userId);
+
+            newAnswer.Content = answerForm.Content;
+            newAnswer.TruncatedContent = answerForm.TruncatedContent;
+            newAnswer.DateCreated = DateTime.Now;
+            newAnswer.AssociatedQuestion = answerForm.AssociatedQuestion;
+
+            _contextService.context.Answer.Add(newAnswer);
         }
 
-        public Answer UpdateAnswer(Answer answer)
+        // POST: AnswerAndAnswerController/Create
+        public void UpdateAnswer(AnswerCreateViewModel vm, int originalId)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            Answer originalAnswer = _contextService.context.Answer
+                .Include(a => a.Author)
+                .Include(a => a.AssociatedQuestion)
+                .SingleOrDefault(a => a.Id == originalId);
+
+            // check if tags were updated to possibly avoid expensive func calls
+            originalAnswer.Content = vm.Content;
+            originalAnswer.TruncatedContent = vm.TruncatedContent;
+            originalAnswer.DateUpdated = DateTime.Now;
+
+            _contextService.context.Answer.Update(originalAnswer);
+            _contextService.context.SaveChanges();
         }
 
         // GET: AnswerAndAnswerController/Edit/5
         public void DeleteAnswer(int id)
         {
-            return View();
+            var question = _contextService.context.Answer.SingleOrDefault(a => a.Id == id);
+
+            _contextService.context.Answer.Remove(question);
+            _contextService.context.SaveChanges();
         }
     }
 }
