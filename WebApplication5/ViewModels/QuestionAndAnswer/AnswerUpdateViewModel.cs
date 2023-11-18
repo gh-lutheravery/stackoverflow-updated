33745720fe26679e200872b8c40f6802a505b770
gh-charles <blakos244@gmail.com>
2023-11-17 21:21:47 -0600

    impl update vms

---
+++ WebApplication5/ViewModels/QuestionAndAnswer/AnswerUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication5.Models;
+
+namespace WebApplication5.ViewModels.QuestionAndAnswer
+{
+    public class AnswerUpdateViewModel
+    {
+        public string Content { get; set; }
+
+        public string TruncatedContent { get; set; }
+
+        public Question? AssociatedQuestion { get; set; }
+
+        public Answer? OriginalAnswer { get; set; }
+    }
+}
---
+++ WebApplication5/ViewModels/QuestionAndAnswer/QuestionUpdateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication5.Models;
+
+namespace WebApplication5.ViewModels.QuestionAndAnswer
+{
+    public class QuestionUpdateViewModel
+    {
+        public List<string> Tags { get; set; }
+
+        public string TagStr { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string TruncatedContent { get; set; }
+
+        public Question? OriginalQuestion { get; set; }
+    }
+}
