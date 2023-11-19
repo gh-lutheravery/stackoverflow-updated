983ec37b024f188ab03cc12cd8579c524d3dfe03
gh-charles <blakos244@gmail.com>
2023-11-18 19:28:23 -0600

    make constants for limits

---
+++ WebApplication5/ViewModels/ValidationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication5.ViewModels
+{
+    public static class ValidationConstants
+    {
+        public const int MaxPostContentLength = 1000000;
+        public const int MaxPostTitleLength = 150;
+
+    }
+}
