4fa7c8eb542e19d7dd93b51f6c7892893da74606
gh-charles <blakos244@gmail.com>
2023-11-18 19:27:46 -0600

    use sort and filter query vars

--- WebApplication5/Controllers/WebControllers/HomeController.cs
+++ WebApplication5/Controllers/WebControllers/HomeController.cs
@@ -7,18 +7,19 @@ namespace WebApplication5.Controllers.WebControllers
     public class HomeController : Controller
     {
         private readonly HomeBusinessController _businessController;
+
         public HomeController(HomeBusinessController businessController) 
         { 
             _businessController = businessController;
         }
 
-        public ActionResult Index(int pageNumber)
+        public ActionResult Index(int pageNumber = 0, string sortBy = "", string filterBy = "")
         {
-            HomeViewModel vm = _businessController.PopulateHomeViewModel(pageNumber);
+            HomeViewModel vm = _businessController.PopulateHomeViewModel(pageNumber, sortBy, filterBy);
             return View(vm);
         }
 
-        public ActionResult SearchResults(string searchQuery, int pageNumber)
+        public ActionResult SearchResults(string searchQuery, int pageNumber = 0)
         {
             SearchViewModel vm = _businessController.PopulateSearchViewModel(pageNumber, searchQuery);
             return View(vm);
