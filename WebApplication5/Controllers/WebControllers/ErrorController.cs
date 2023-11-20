39dab113a6350f57e14590e6fa7abef761ed274f
gh-charles <blakos244@gmail.com>
2023-11-19 19:14:48 -0600

    make err ctr

---
+++ WebApplication5/Controllers/WebControllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers.WebControllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("not-found")]
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
