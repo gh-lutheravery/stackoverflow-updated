58200651120437622cc8853964d2bb634dbbe32d
gh-charles <blakos244@gmail.com>
2023-11-21 17:55:33 -0600

    revise question table

---
+++ WebApplication5/Migrations/20231120012517_ReviseQuestionTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication5.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReviseQuestionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AcceptedAnswerId",
+                table: "Question",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AnswerCount",
+                table: "Question",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptedAnswerId",
+                table: "Question");
+
+            migrationBuilder.DropColumn(
+                name: "AnswerCount",
+                table: "Question");
+        }
+    }
+}
