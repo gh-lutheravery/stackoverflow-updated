39edcd0bf4c4b757843c9fbb57a5429866d3a9ae
gh-charles <blakos244@gmail.com>
2023-11-21 18:10:55 -0600

    extend host with db

---
+++ WebApplication5/Data/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication5.Data
+{
+    public static class Extension
+    {
+        public static void CreateDevDbIfNotExists(this IHost host)
+        {    
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<StackOverflowCloneContext>();
+                //context.Database.EnsureCreated();
+                DevDbInitializer.Initialize(context);
+            } 
+        }
+    }
+}
