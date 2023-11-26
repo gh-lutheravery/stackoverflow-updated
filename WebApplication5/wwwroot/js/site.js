4795f62d3c5d36de9cb77b528bbd2ccaf89ce537
gh-charles <blakos244@gmail.com>
2023-11-26 00:17:43 -0600

    add modal js in site wwwroot

--- WebApplication5/wwwroot/js/site.js
+++ WebApplication5/wwwroot/js/site.js
@@ -1,4 +1,104 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Requires jquery for vote count functions
 
-// Write your JavaScript code.
+
+// Functions for modals used for various CRUD views
+function openModal(modal) {
+    modal.classList.add('is-active');
+}
+
+function closeModal(modal) {
+    modal.classList.remove('is-active');
+}
+
+// Add a click event on buttons to open a specific modal
+(document.querySelectorAll('.open-modal-trigger') || []).forEach((trigger) => {
+    const modal = trigger.dataset.target;
+    const target = document.getElementById(modal);
+
+    trigger.addEventListener('click', () => {
+        openModal(target);
+    });
+});
+
+// Add a click event on various child elements to close the parent modal
+(document.querySelectorAll('.cancel-modal-trigger') || []).forEach((close) => {
+    /*const target = close.closest('.modal');*/
+    const modal = trigger.dataset.target;
+    const target = document.getElementById(modal);
+
+    close.addEventListener('click', () => {
+        closeModal(target);
+    });
+});
+
+
+
+// Functions for voting in QuestionAnswer view
+function handleQuestionVote(element, color, vote) {
+    // vote is true when upvoting, and vice versa
+    element.css('background-color', color);
+
+    incrementVoteCount($('#ques-vote-count'), vote);
+
+    // $.ajax({
+    //     url: '@Url.Action("UpdateVote", "Question")',
+    //     method: 'POST',
+    //     data: { questionId: @Model.Question.Id, upVote: vote },
+    //     contentType: "application/json; charset=utf-8",
+    //     dataType: "json",
+    //     error: function() {
+    //         console.log('Error voting post.');
+    //     }
+    // });
+}
+
+function handleAnswerVote(element, color, vote) {
+    element.css('background-color', color);
+
+    let voteContainer = element.parent().parent();
+    let voteCountEl = voteContainer.children('.ans-vote-count')[0];
+
+    incrementVoteCount(voteCountEl, vote);
+
+    // $.ajax({
+    //     url: '@Url.Action("UpdateVote", "Answer")',
+    //     method: 'POST',
+    //     data: { answerId: @Model.Question.Id, upVote: vote },
+    //     contentType: "application/json; charset=utf-8",
+    //     dataType: "json",
+    //     error: function() {
+    //         console.log('Error voting post.');
+    //     }
+    // });
+}
+
+function incrementVoteCount(element, vote) {
+    let voteCount = Number(element.text());
+
+    if (vote) {
+        element.text(
+            String(voteCount + 1)
+        );
+    }
+    else {
+        element.text(
+            String(voteCount - 1)
+        );
+    }
+}
+
+$('#ques-up-vote').click(function () {
+    handleQuestionVote($(this), "#0066ffff", true)
+});
+
+$('#ques-down-vote').click(function () {
+    handleQuestionVote($(this), "#42414cff", false)
+});
+
+$('.ans-up-vote').click(function () {
+    handleAnswerVote($(this), "#0066ffff", true)
+});
+
+$('.ans-down-vote').click(function () {
+    handleAnswerVote($(this), "#42414cff", false)
+});
