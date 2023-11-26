9ed34fdf2238963b30be88044258ada7f6bab4d9
gh-charles <blakos244@gmail.com>
2023-11-26 00:12:13 -0600

    style register view

--- WebApplication5/ViewModels/User/RegisterViewModel.cs
+++ WebApplication5/ViewModels/User/RegisterViewModel.cs
@@ -12,11 +12,11 @@ namespace WebApplication5.ViewModels.User
         public string Email { get; set; }
 
         [Required]
-        [MinLength(8)]
+        [MinLength(8, ErrorMessage = "Password length must be more than 8 characters.")]
         public string Password { get; set; }
 
         [Required]
-        [Compare(nameof(Password))]
+        [Compare(nameof(Password), ErrorMessage = "Make sure your password confirmation matches your password.")]
         public string ConfirmPassword { get; set; }
     }
 }
