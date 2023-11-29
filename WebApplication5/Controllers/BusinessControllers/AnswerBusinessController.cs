b7ae0e6f3de692cc40e7be0eac1d529df1feda97
gh-charles <blakos244@gmail.com>
2023-11-28 22:37:03 -0600

    fix ef bug

--- WebApplication5/Controllers/BusinessControllers/AnswerBusinessController.cs
+++ WebApplication5/Controllers/BusinessControllers/AnswerBusinessController.cs
@@ -23,33 +23,37 @@ namespace WebApplication5.Controllers.BusinessControllers
             Answer newAnswer = new Answer();
 
             int userId = Int32.Parse(userCookie.FindFirstValue("ID"));
-            newAnswer.Author = _contextService.GetProfileById(userId);
+            newAnswer.Author = _contextService.context.Profile.Find(userId);
 
             newAnswer.Content = answerForm.Content;
             newAnswer.TruncatedContent = answerForm.TruncatedContent;
             newAnswer.DateCreated = DateTime.Now;
-            newAnswer.AssociatedQuestion = answerForm.AssociatedQuestion;
+
+            var associatedQuestion = _contextService.context.Question.Find(answerForm.AssociatedQuestionId);
+            newAnswer.AssociatedQuestion = associatedQuestion;
 
             _contextService.context.Answer.Add(newAnswer);
-            _questionBusiness.IncrementAnswerCount(answerForm.AssociatedQuestion.Id);
             _contextService.context.SaveChanges();
+
+            _questionBusiness.IncrementAnswerCount(answerForm.AssociatedQuestionId);
         }
 
         // POST: AnswerAndAnswerController/Create
         public void UpdateAnswer(AnswerUpdateViewModel vm)
         {
-            vm.OriginalAnswer.Content = vm.Content;
-            vm.OriginalAnswer.TruncatedContent = vm.TruncatedContent;
-            vm.OriginalAnswer.DateUpdated = DateTime.Now;
+            Answer answer = _contextService.context.Answer.Find(vm.OriginalAnswerId);
 
-            _contextService.context.Answer.Update(vm.OriginalAnswer);
+            answer.Content = vm.Content;
+            answer.DateUpdated = DateTime.Now;
+
+            _contextService.context.Answer.Update(answer);
             _contextService.context.SaveChanges();
         }
 
         // GET: AnswerAndAnswerController/Edit/5
         public void DeleteAnswer(int id)
         {
-            var question = _contextService.context.Answer.SingleOrDefault(a => a.Id == id);
+            var question = _contextService.context.Answer.Find(id);
 
             _contextService.context.Answer.Remove(question);
             _contextService.context.SaveChanges();
