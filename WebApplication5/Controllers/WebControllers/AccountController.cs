1719b4082160bc0f956df0a651992ef9effdac70
gh-charles <blakos244@gmail.com>
2023-12-01 18:40:18 -0600

    add account ctr

---
+++ WebApplication5/Controllers/WebControllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers.WebControllers
+{
+    // redirects the default ASP.NET auth paths; necessary because they can't seem to be changed in this version
+
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return RedirectToAction("Register", "Profile");
+        }
+
+        public ActionResult Login()
+        {
+            return RedirectToAction("Login", "Profile");
+        }
+
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Logout", "Profile");
+        }
+
+        public ActionResult AccessDenied()
+        {
+            return RedirectToAction("Forbidden", "Error");
+        }
+    }
+}
