53b15bc00bda4127e2351297f5b26594c48db91f
gh-charles <blakos244@gmail.com>
2023-12-03 16:05:35 -0600

    make email column unique

---
+++ WebApplication5/Migrations/20231203220421_UniqueEmail.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication5.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Profile",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_Email",
+                table: "Profile",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Profile_Email",
+                table: "Profile");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Profile",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
